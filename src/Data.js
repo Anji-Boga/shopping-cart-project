29d23d4b0f12a02daaaa6f31b3eaec53d59c3e3a
Anji-Boga <143120482+Anji-Boga@users.noreply.github.com>
2023-10-11 23:21:16 +0530

    Add files via upload

---
+++ src/Data.js
@@ -0,0 +1,58 @@
+let shopItemsData = [
+    {
+      id: "jfhgbvnscs",
+      name: "Casual Shirt",
+      price: 45,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-5.jpg",
+    },
+    {
+      id: "ioytrhndcv",
+      name: "Office Shirt",
+      price: 100,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-6.jpg",
+    },
+    {
+      id: "wuefbncxbsn",
+      name: "T Shirt",
+      price: 25,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-3.jpg",
+    },
+    {
+      id: "thyfhcbcv",
+      name: "Mens Suit",
+      price: 300,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-4.jpg",
+    },
+    {
+      id: "ijkjlmopqs",
+      name: "Shorts",
+      price: 40,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-7.jpg",
+    },
+    {
+      id: "qawesdfopyue",
+      name: "Green shirt",
+      price: 25,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-1.jpg",
+    },
+    {
+      id: "asdnnqwmkli",
+      name: "Blue Jeans",
+      price: 100,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-8.jpg",
+    },
+    {
+      id: "mnbzytcvsdfyue",
+      name: "Formal shirts",
+      price: 50,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-2.jpg",
+    }
+  ];
